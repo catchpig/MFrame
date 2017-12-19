4f61d618a02faebbc3c1dc547c413dc192a9a061
beaven <beaven_wang@163.com>
2017-12-19 21:50:44 +0800

    delete

---
+++ library/src/main/java/mejust/frame/BaseApplication.java
@@ -0,0 +1,12 @@
+package mejust.frame;
+
+import android.app.Application;
+
+/**
+ * @author : Beaven
+ * @date : 2017-12-19 21:42
+ */
+
+public abstract class BaseApplication extends Application {
+
+}
