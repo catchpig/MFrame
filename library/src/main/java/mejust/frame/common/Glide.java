12eefe400452b736405ef57c4c52ea49bdf00e3b
beaven <beaven_wang@163.com>
2017-12-20 14:32:56 +0800

    引入Glide框架

--- library/src/main/java/mejust/frame/common/Glide.java
+++
@@ -1,9 +0,0 @@
-package mejust.frame.common;
-
-/**
- * @author : Beaven
- * @date : 2017-12-20 14:31
- */
-
-public class Glide {
-}
---
+++ library/src/main/java/mejust/frame/image/Glide.java
@@ -0,0 +1,9 @@
+package mejust.frame.image;
+
+/**
+ * @author : Beaven
+ * @date : 2017-12-20 14:32
+ */
+
+public class Glide {
+}
