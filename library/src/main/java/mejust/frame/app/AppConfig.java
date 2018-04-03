d222298e377ced377f6666f6a4bf319df5e623d1
tao <327785185@qq.com>
2018-04-03 11:12:16 +0800

    将AppConfig调整为接口

--- library/src/main/java/mejust/frame/app/AppConfig.java
+++ library/src/main/java/mejust/frame/app/AppConfig.java
@@ -6,40 +6,40 @@ package mejust.frame.app;
  * <p>
  * 系统固定字段定义，上层应用可继承，添加字段
  */
-public class AppConfig {
+public interface AppConfig {
 
     /**
      * 程序运行状态区分
      */
-    public static boolean DEBUG = false;
+    boolean DEBUG = false;
 
     /**
      * 网络请求详情打印
      */
-    public static String URL_LOG = "OkHttp";
+    String URL_LOG = "OkHttp";
 
     /**
      * 连接超时，单位：秒
      */
-    public static long CONNECT_TIME_OUT_DEFAULT = 10;
+    long CONNECT_TIME_OUT_DEFAULT = 10;
 
     /**
      * 读取超时，单位：秒
      */
-    public static long READ_TIME_OUT_DEFAULT = 10;
+    long READ_TIME_OUT_DEFAULT = 10;
 
     /**
      * 写入超时，单位：秒
      */
-    public static long WRITE_TIME_OUT_DEFAULT = 10;
+    long WRITE_TIME_OUT_DEFAULT = 10;
 
     /**
      * json解析时间格式
      */
-    public static String DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
+    String DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
 
     /**
      * 网络状态监听标志判断
      */
-    public static boolean NETWORK_STATUS_MONITORING = true;
+    boolean NETWORK_STATUS_MONITORING = true;
 }
