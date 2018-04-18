4b0999e86c6c5a3ed98dc48155537fd801b8d813
Beaven <beaven_wang@163.com>
2018-04-18 18:12:32 +0800

    测试

--- app/src/main/java/com/zhuazhu/frame/mvp/activity/PuppetActivity.java
+++ app/src/main/java/com/zhuazhu/frame/mvp/activity/PuppetActivity.java
@@ -2,27 +2,27 @@ package com.zhuazhu.frame.mvp.activity;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.widget.ImageView;
+import android.view.View;
 import com.zhuazhu.frame.R;
 import mejust.frame.annotation.LayoutId;
-import mejust.frame.image.ImageUtils;
+import mejust.frame.annotation.StatusBarConfig;
 import mejust.frame.mvp.view.BaseActivity;
+import mejust.frame.widget.title.StatusBar;
 
 /**
  * @author wangpeifeng
  * @date 2018/04/18 17:12
  */
 @LayoutId(R.layout.activity_puppet)
+@StatusBarConfig(isInitActivity = false)
 public class PuppetActivity extends BaseActivity {
     private String url = "http://image.tianjimedia.com/uploadImages/2015/285/24/586K2UOWHG9D.jpg";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //getSupportFragmentManager().beginTransaction()
-        //        .add(R.id.fragment, new PuppetFragment())
-        //        .commitAllowingStateLoss();
-        ImageView imageView = findViewById(R.id.image);
-        ImageUtils.show(imageView, url);
+        mTitleBar.setVisibility(View.GONE);
+        mStatusBar = StatusBar.with(this);
+        mStatusBar.init();
     }
 }
